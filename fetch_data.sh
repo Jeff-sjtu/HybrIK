3707559737b4fc191685c55dd78636ab52eaabb4
Jeff-sjtu <jeff.lee.sjtu@gmail.com>
2022-04-27 00:08:02 +0800

    update fatch_data

--- fetch_data.sh
+++ fetch_data.sh
@@ -10,5 +10,5 @@ password=$(urle $password)
 
 wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplify&sfile=mpips_smplify_public_v2.zip&resume=1' -O '/content/HybrIK/mpips_smplify_public_v2.zip' --no-check-certificate --continue
 unzip mpips_smplify_public_v2.zip -d smplx_files
-mv mpips_smplify_public_v2/code/models/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl ./model_files
+mv smplx_files/smplify_public/code/models/basicModel_neutral_lbs_10_207_0_v1.0.0.pkl ./model_files
 rm -rf *.zip mpips_smplify_public_v2 
